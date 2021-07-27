746a9732a2127fa0e4accd40d8b6df458f05382d
lk281 <laurenkwan03@gmail.com>
2021-07-27 23:22:29 +0000

    changed python to correspond with html variable names and fixed proxy error

--- app.py
+++ app.py
@@ -5,44 +5,29 @@ from datetime import datetime
 from flask_pymongo import PyMongo
 # from model import getImageUrlFrom
 import os
+from model import question_list
 
 # -- Initialization section --
 app = Flask(__name__)
-
-#each question is its own dictionary then they are added to a question list 
-question1 = {"question": "What is money?", "wrong_answers": ["another word for dollars", "the paper version of coins"], 
-"correct_answer": "an asset that people (or institutions) possess, which can be exchanged for goods and services at a certain rate (the price)."}
-
-question2 = {"question": "What is one thing a loan should be used for as a student?", "wrong_answers": ["Paying for a vacation", "Paying for new expensive clothes"], "correct_answer": "Paying for college"}
-
-question3 = {"question": "What is FASFA?", "wrong_answers": [" A loan issued by the government to help pay for college", "The Fast Food Association scholarship that aids in paying off student loans"], "correct_answer": " A Free Application for Federal Student Aid used by college students to determine their eligibility for student financial aid."}
-
-question_list = [question1, question2, question3]
 
 user_answers = {"question1": "", "question2": "", "question3": ""}
 
 # -- Routes section --
 
-
-# this code is for the homepage (start screen)
 @app.route('/')
-@app.route('/homepage', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 
 # first page that shows up when the app is run
 def homepage():
-    return render_template('homepage.html')
+    return render_template('index.html')
 
-# currently we only have 3 questions - as we put more in the data base we will add more app routes 
+
 @app.route('/')
 @app.route('/question1', methods=['POST', 'GET'])
 
 def question_one():
     question = question_list[0]
-    if request.method == 'POST': 
-        answer = request.form['answer']
-        print("here: "+answer)
     return render_template('question1.html', question = question)
-
 
 
 @app.route('/')
@@ -51,9 +36,8 @@ def question_one():
 def question_two():
     question = question_list[1]
     if request.method == 'POST': 
-        answer = request.form['answer']
-        print("here2: "+answer)
-        if answer == "correct":
+        answer = request.form['Q1']
+        if answer == "answer3":
             user_answers["question1"] = "correct"
         else: 
             user_answers["question1"] = "wrong"
@@ -66,9 +50,8 @@ def question_two():
 def question_three():
     question = question_list[2]
     if request.method == 'POST': 
-        answer = request.form['answer']
-        print("here3: "+answer)
-        if answer == "correct":
+        answer = request.form['Q1']
+        if answer == "answer6":
             user_answers["question2"] = "correct"
         else: 
             user_answers["question2"] = "wrong"
@@ -76,31 +59,20 @@ def question_three():
 
 
 @app.route('/')
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/results', methods=['GET', 'POST'])
 
 def result():
 
     if request.method == 'POST': 
-        answer = request.form['answer']
-        if answer == "correct":
+        answer = request.form["Q1"]
+        if answer == "answer9":
             user_answers["question3"] = "correct"
         else: 
             user_answers["question3"] = "wrong"
-    # initialize the score variable to keep track of score
-    score = 0
-    # this is what answer.keys() is => [question1, question2, question3]
-    # a list of keys for the dictionary user_answers
-    # we loop through the keys
-    for key in user_answers.keys(): 
-        # print(user_answers)
-        # ex. for the first loop key = "question1"
-        # so we index into our dictionary (user_answers) with the key:
-        # user_answers["question1"]
 
-        # we index into the dictionary with the key to check the value at that key 
-        # we check if the user got the question right or wrong
-        # if it was right we add 1 to the score 
+    score = 0
+
+    for key in user_answers.keys(): 
         if user_answers[key] == "correct":
             score += 1
-    # after calculating we send the score to the results.html file 
     return render_template('results.html', score = score)
---
+++ model.py
@@ -0,0 +1,9 @@
+#each question is its own dictionary then they are added to a question list 
+question1 = {"question": "What is money?", "wrong_answers": ["another word for dollars", "the paper version of coins"], 
+"correct_answer": "an asset that people (or institutions) possess, which can be exchanged for goods and services at a certain rate (the price)."}
+
+question2 = {"question": "What is one thing a loan should be used for as a student?", "wrong_answers": ["Paying for a vacation", "Paying for new expensive clothes"], "correct_answer": "Paying for college"}
+
+question3 = {"question": "What is FASFA?", "wrong_answers": [" A loan issued by the government to help pay for college", "The Fast Food Association scholarship that aids in paying off student loans"], "correct_answer": " A Free Application for Federal Student Aid used by college students to determine their eligibility for student financial aid."}
+
+question_list = [question1, question2, question3]
